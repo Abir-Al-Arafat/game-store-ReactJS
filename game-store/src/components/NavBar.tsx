e1afe1f20067e9fb82c7994955a3f6e48f7d22e2
Abir-Al-Arafat <antorabir@gmail.com>
2023-06-14 23:16:45 +0600

    Dark/Light Mode Switch added

--- game-store/src/components/NavBar.tsx
+++ game-store/src/components/NavBar.tsx
@@ -1,14 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <div>
-      <HStack>
-        <Image src={logo} boxSize="60px"></Image>
-        <Text>Navbar</Text>
-      </HStack>
-    </div>
+    <HStack justifyContent="space-between" padding="0 10px">
+      <Image src={logo} boxSize="60px"></Image>
+      <ColorModeSwitch />
+    </HStack>
   );
 };
 
