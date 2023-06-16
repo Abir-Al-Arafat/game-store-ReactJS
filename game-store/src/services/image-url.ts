25f2b143402d55c7cde64d22a376b3a81971bf80
Abir-Al-Arafat <antorabir@gmail.com>
2023-06-17 02:46:46 +0600

    image crop function added
    an image crop function added so that images can be cropped to a default height and width while fetching from the URL

---
+++ game-store/src/services/image-url.ts
@@ -0,0 +1,13 @@
+
+// function for getting a cropped image from the URL
+const getCroppedImageUrl = (url: string) => {
+  // parameter adding point   
+  const target = 'media/'
+  // index of this portion: https://media.rawg.io/media/   
+  const index = url.indexOf(target) + target.length
+
+  // https://media.rawg.io/media/crop/600/400/games/456/456dea5e1c7e3cd07060c14e96612001.jpg
+  return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
+}
+
+export default getCroppedImageUrl
