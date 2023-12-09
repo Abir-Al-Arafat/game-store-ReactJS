03ae11a87c3f5b3706819ba7407b1f0e47d05f7d
abir-bjit <abir.arafat@bjitacademy.com>
2023-12-09 21:11:50 +0600

    Update api-client.ts

--- game-store/src/services/api-client.ts
+++ game-store/src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params:{
-        key: '75e3fec8851a4a6aadd9ddb439c96a4c'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: import.meta.env.VITE_APIKEY,
+  },
+});
