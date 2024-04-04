34ac6d46da09e3c025bb7191f361fe3f4d175c2e
Abir-Al-Arafat <antorabir@gmail.com>
2024-04-04 15:47:40 +0600

    react query added for fetching platforms

--- game-store/src/components/PlatformSelector.tsx
+++ game-store/src/components/PlatformSelector.tsx
@@ -10,6 +10,7 @@ interface Props {
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   const { data, error } = usePlatforms();
+  console.log(data);
 
   if (error) return null;
   return (
@@ -18,7 +19,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
             key={platform.id}
--- game-store/src/hooks/usePlatforms.ts
+++ game-store/src/hooks/usePlatforms.ts
@@ -1,11 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import useData from "./useData";
+import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
-interface Platform{
-    id: number;
-    name: string;
-    slug: string;
+interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+// const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: platforms,
+  });
 
-export default usePlatforms
+export default usePlatforms;
