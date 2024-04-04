84410cb837b5dee7036bbaaa5981549b737a0631
Abir-Al-Arafat <antorabir@gmail.com>
2024-04-04 14:01:38 +0600

    genre has been fetched using react query

--- game-store/src/hooks/useGenres.ts
+++ game-store/src/hooks/useGenres.ts
@@ -1,11 +1,21 @@
-import useData from './useData';
+import useData, { FetchResponse } from "./useData";
+import genres from "../data/genres";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
 
-export interface Genre{
-    id: number;
-    name: string;
-    image_background: string;
+export interface Genre {
+  id: number;
+  name: string;
+  image_background: string;
 }
 
-const useGenres = () => useData<Genre>('/genres')
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: genres,
+  });
 
-export default useGenres
+export default useGenres;
