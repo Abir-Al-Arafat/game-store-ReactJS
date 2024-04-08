12b70d65a7cbd552271b22d432b8316869dbc338
Abir-Al-Arafat <antorabir@gmail.com>
2024-04-08 19:29:13 +0600

    library added for simplifying time calculation

--- game-store/src/hooks/useGenres.ts
+++ game-store/src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
-import { FetchResponse } from "../services/api-client";
-import genres from "../data/genres";
+import ms from "ms";
 import { useQuery } from "@tanstack/react-query";
+import genres from "../data/genres";
 import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Genre>("/genres");
@@ -15,7 +15,7 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24h
+    staleTime: ms("24h"),
     initialData: genres,
   });
 
--- game-store/src/hooks/usePlatforms.ts
+++ game-store/src/hooks/usePlatforms.ts
@@ -1,3 +1,4 @@
+import ms from "ms";
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import APIClient, { FetchResponse } from "../services/api-client";
@@ -15,7 +16,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24h
+    staleTime: ms("24h"),
     initialData: platforms,
   });
 
